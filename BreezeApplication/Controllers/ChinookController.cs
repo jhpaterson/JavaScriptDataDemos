07f5d282f6611728fdef1cd860c042d6fb626883
Jim Paterson <James.Paterson@gcu.ac.uk>
2013-03-03 23:04:45 +0000

    initial commit

---
+++ BreezeApplication/Controllers/ChinookController.cs
@@ -0,0 +1,49 @@
+﻿using Breeze.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using BreezeApplication.Models;
+using Newtonsoft.Json.Linq;
+
+namespace BreezeApplication.Controllers
+{
+    [BreezeController]
+    public class ChinookController : ApiController
+    {
+        readonly EFContextProvider<ChinookEntities> _contextProvider =
+            new EFContextProvider<ChinookEntities>();
+
+        // ~/api/chinook/Metadata 
+        [HttpGet]
+        public string Metadata()
+        {
+            return _contextProvider.Metadata();
+        }
+
+        // ~/api/chinook/artists
+        // ~/api/chinool/artists?$filter=Name%20startswith%20L&$orderby=LastName 
+        [HttpGet]
+        public IQueryable<Artist> Artists()
+        {
+            return _contextProvider.Context.Artists;
+        }
+
+        // ~/api/breezydevices/devices
+        [HttpGet]
+        public IQueryable<Album> Albums()
+        {
+            return _contextProvider.Context.Albums;
+        }
+
+        // ~/api/chinook/SaveChanges
+        [HttpPost]
+        public SaveResult SaveChanges(JObject saveBundle)
+        {
+            return _contextProvider.SaveChanges(saveBundle);
+        }
+    }
+}
---
+++ ChinookService/ChinookDataService.svc.cs
@@ -0,0 +1,27 @@
+//------------------------------------------------------------------------------
+// <copyright file="WebDataService.svc.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+using ChinookService.Models;
+
+namespace ChinookService
+{
+    public class ChinookDataService : DataService<ChinookEntities>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            config.SetEntitySetAccessRule("*", EntitySetRights.All);
+            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+        }
+    }
+}
